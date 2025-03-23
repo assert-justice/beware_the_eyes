b2216ae83136485adc9d9cd97e7527ce2abca355
Riley <waskerdu@gmail.com>
2025-03-23 11:57:53 -0700

    add shotgun alt fire and sounds

--- scripts/game/weapons/Shotgun.cs
+++ scripts/game/weapons/Shotgun.cs
@@ -2,33 +2,42 @@ using Godot;
 
 public partial class Shotgun : Weapon
 {
-	[Export] float FireTime = 0.5f;
+	[Export] float FireTime = 1.5f;
+	[Export] float AltFireTime = 0.5f;
 	[Export] float Damage = 50;
+	[Export] float DamageMul = 2;
 	[Export] PackedScene SparkleScene;
 	Clock fireClock;
+	Clock altFireClock;
 	EntPool sparklePool;
+	AudioStreamPlayer3D fireSound;
+	AudioStreamPlayer3D altFireSound;
 	public override void _Ready()
 	{
 		base._Ready();
 		var parent = GetTree().GetNodesInGroup("Game")[0];
-		fireClock = AddClock(FireTime, 0);
 		sparklePool = AddPool(parent, ()=>SparkleScene.Instantiate<Entity>());
+		fireClock = AddClock(FireTime, 0);
+		altFireClock = AddClock(AltFireTime, 0);
+		fireSound = GetNode<AudioStreamPlayer3D>("FireSound");
+		altFireSound = GetNode<AudioStreamPlayer3D>("AltFireSound");
 	}
 	public override void TryFire(FireCommand command)
 	{
+		if(fireClock.IsRunning()) return;
+		if(altFireClock.IsRunning()) return;
 		if(CanFire(command)) Fire(command);
 		else if(CanAltFire(command)) AltFire(command);
 	}
 	bool CanFire(FireCommand command){
-		if(fireClock.IsRunning()) return false;
 		return command.FireJustPressed;
 	}
 	bool CanAltFire(FireCommand command){
-		if(fireClock.IsRunning()) return false;
 		return command.AltJustPressed;
 	}
 	void Fire(FireCommand command){
 		fireClock.Reset();
+		fireSound.Play();
 		if(command.Ray.IsColliding()){
 			var pos = command.Ray.GetCollisionPoint();
 			var s = sparklePool.GetPool().GetNew();
@@ -38,5 +47,16 @@ public partial class Shotgun : Weapon
 			}
 		}
 	}
-	void AltFire(FireCommand command){}
+	void AltFire(FireCommand command){
+		altFireClock.Reset();
+		altFireSound.Play();
+		if(command.Ray.IsColliding()){
+			var pos = command.Ray.GetCollisionPoint();
+			var s = sparklePool.GetPool().GetNew();
+			s.Position = pos;
+			if(command.Ray.GetCollider() is Actor actor){
+				actor.Damage(Damage * DamageMul);
+			}
+		}
+	}
 }
