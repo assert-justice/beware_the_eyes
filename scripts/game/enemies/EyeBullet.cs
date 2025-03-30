6f224dc5c4f67b07f46bf58151696e08f7f71ef7
Riley <waskerdu@gmail.com>
2025-03-29 20:11:56 -0700

    remove eye bullets on collision

--- scripts/game/enemies/EyeBullet.cs
+++ scripts/game/enemies/EyeBullet.cs
@@ -3,15 +3,27 @@ using System;
 
 public partial class EyeBullet : Entity
 {
-    [Export] public float Damage = 10;
-    public override void _Ready()
-    {
-        base._Ready();
-        Team = 1;
-        GetNode<Area3D>("Area3D").BodyEntered += b => {
-            if(b is Actor a && a.GetTeam() != GetTeam()){
-                a.Damage(Damage);
-            }
-        };
-    }
+	[Export] public float Damage = 10;
+	Clock lifetime;
+	public override void _Ready()
+	{
+		base._Ready();
+		Team = 1;
+		GetNode<Area3D>("Area3D").BodyEntered += b => {
+			if(b is Actor a){
+				if(a.GetTeam() != GetTeam()){
+                    a.Damage(Damage);
+                    CallDeferred("Die");
+                }
+			}
+			else CallDeferred("Die");
+		};
+		lifetime = AddClock(10);
+		lifetime.Timeout = Die;
+	}
+	public override void Init()
+	{
+		base.Init();
+		lifetime.Reset();
+	}
 }
